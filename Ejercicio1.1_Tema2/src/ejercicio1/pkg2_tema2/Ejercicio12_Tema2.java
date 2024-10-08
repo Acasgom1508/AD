42b875c24d3d77d3fb24eb1b2fb989504d8eb3f1
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-08 09:54:13 +0200

    Terminado el ejercicio 1.1 del tema 2

---
+++ Ejercicio1.1_Tema2/src/ejercicio1/pkg2_tema2/Ejercicio12_Tema2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1.pkg2_tema2;
+
+import java.io.File;
+
+/**
+ *
+ * @author DAM_M
+ */
+public class Ejercicio12_Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File fichero = new File(args[0]);
+        if (fichero.isDirectory()) {
+            File[] archivos = fichero.listFiles();
+            for (File archivo : archivos) {
+                System.out.println("Nombre: " + archivo.getName());
+            }
+            
+        }else System.out.println("No es un directorio");
+    }
+    
+}
