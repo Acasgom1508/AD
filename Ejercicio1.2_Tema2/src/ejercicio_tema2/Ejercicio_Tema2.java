18a39efd74d5c20e3dc4513f267277e891fa4757
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-08 09:54:35 +0200

    Terminado el ejercicio 1.2 del tema 2

---
+++ Ejercicio1.2_Tema2/src/ejercicio_tema2/Ejercicio_Tema2.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_tema2;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM_M
+ */
+public class Ejercicio_Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ruta del directorio: ");
+        File fichero = new File(sc.nextLine());
+        System.out.println("-------------------------------");
+        System.out.println("Es un directorio");
+            String tipo = "";
+            String leerEscribir = "";
+            if (fichero.canRead()) {
+                if (fichero.canWrite()) {
+                    leerEscribir = "Si";
+                } else {
+                    leerEscribir = "Solo puede leer";
+                }
+            } else if (fichero.canWrite()) {
+                leerEscribir = "Solo puede escribir";
+            }
+            System.out.println("Nombre:" + fichero.getName());
+            System.out.println("Ruta absoluta: " + fichero.getAbsolutePath());
+            System.out.println("Tamaño; " + fichero.length() + " bytes");
+
+            System.out.println("Se puede leer y escribir?: " + leerEscribir);
+            if (fichero.isDirectory()) {
+                tipo = "Directorio";
+            } else {
+                tipo = "Archivo";
+            }
+            
+            System.out.println("Es fichero o directorio?: " + tipo);
+            System.out.println("Nombre de su directorio padre: " + fichero.getParent());
+            System.out.println("-------------------------------");
+
+    }
+
+}
