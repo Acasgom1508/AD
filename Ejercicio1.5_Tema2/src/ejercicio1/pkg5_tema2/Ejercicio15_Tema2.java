a036bdc7ae7c29af41ffecabd23f2a8c3c3fe744
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-08 11:26:54 +0200

    Terminado el apartado 5 del tema 2

---
+++ Ejercicio1.5_Tema2/src/ejercicio1/pkg5_tema2/Ejercicio15_Tema2.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1.pkg5_tema2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM_M
+ */
+public class Ejercicio15_Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("nombres.txt"));
+            System.out.print("Nombre: ");
+            String nombre = sc.nextLine();
+
+            System.out.print("Edad: ");
+            int edad = sc.nextInt();
+            String linea = nombre + ", " + edad + " años";
+            System.out.println("Añadida nueva línea: " + linea);
+            bw.write(linea);
+        } catch (IOException e) {
+            System.out.println("No se ha podido escribir en el fichero");
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            }catch (IOException io) {
+                System.out.println("No se ha cerrado");
+            }
+        }
+
+    }
+
+}
