fa5f4885f64b538fd0aad9a8003604dc42bd4f20
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-14 19:13:37 +0200

    Termino el punto 1.6

---
+++ Ejercicio1.6_Tema2/src/ejercicio1/pkg6_tema2/Ejercicio16_Tema2.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1.pkg6_tema2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejercicio16_Tema2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner arch = null;
+
+        String archivo = null;
+        do {
+            System.out.print("Ruta del archivo a leer: ");
+            archivo = sc.nextLine();
+        } while (archivo.isEmpty());
+
+        try {
+            arch = new Scanner(new FileReader(archivo));
+            while (arch.hasNext()) {
+                System.out.print(arch.nextLine());
+            }
+            System.out.println("\n");
+        } catch (IOException e) {
+            System.out.println("No se ha podido leer el archivo");
+        } finally {
+            try {
+                if (arch != null) {
+                    arch.close();
+                }
+            } catch (Exception e) {
+                System.out.println("No se ha cerrado correctamente");
+            }
+        }
+    }
+}
