da44886e7dc7744d97139d7d754226aaa876ce7a
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-15 12:10:24 +0200

    Creo el apartado 2.1 y creo unas variables

---
+++ Ejercicio2.1_Tema2/src/ejercicio2/pkg1_tema2/Ejercicio1_2Tema2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2.pkg1_tema2;
+
+/**
+ *
+ * @author DAM_M
+ */
+public class Ejercicio1_2Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
