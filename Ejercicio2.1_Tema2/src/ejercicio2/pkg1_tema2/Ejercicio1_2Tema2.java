1b645d53d27cdeeae6825b2f7cbd3f2fb22bea24
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-17 14:53:15 +0200

    Update Ejercicio1_2Tema2.java

--- Ejercicio2.1_Tema2/src/ejercicio2/pkg1_tema2/Ejercicio1_2Tema2.java
+++ Ejercicio2.1_Tema2/src/ejercicio2/pkg1_tema2/Ejercicio1_2Tema2.java
@@ -4,6 +4,16 @@
  */
 package ejercicio2.pkg1_tema2;
 
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author DAM_M
@@ -13,8 +23,15 @@ public class Ejercicio1_2Tema2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SAXException, ParserConfigurationException, IOException {
+        /*Dado el archivo películas.xml que se adjunta y con la ayuda de DOM, se pide mostrar:
+        • El título de la película,
+        • Año,
+        • Director
+        • Los nombres de los actores principales.*/
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse("peliculas.xm");
     }
-    
+
 }
