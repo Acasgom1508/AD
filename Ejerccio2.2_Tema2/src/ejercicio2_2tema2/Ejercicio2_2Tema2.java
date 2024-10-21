bbfc03de6b398e52147331e1342393d2e4d17f6d
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-21 08:56:51 +0200

    Hechos el 2.1 y 2.2 en tiempo record

---
+++ Ejerccio2.2_Tema2/src/ejercicio2_2tema2/Ejercicio2_2Tema2.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2_2tema2;
+
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author DAM
+ */
+public class Ejercicio2_2Tema2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        Document document = null;
+        try {
+            DocumentBuilder builder = builderFactory.newDocumentBuilder();
+            File f = new File("Productos.xml");
+            document = builder.parse(f);
+            Element doc = document.getDocumentElement();
+            NodeList nlp = doc.getElementsByTagName("Producto");
+            for (int i = 0; i < nlp.getLength(); i++) {
+                Element prod = (Element) nlp.item(i);
+                Element stock = (Element) prod.getElementsByTagName("Stock").item(0);
+                if (Integer.parseInt(stock.getTextContent().trim()) < 5) {
+                    prod.setAttribute("aLaVenta", "false");
+                }
+            }
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File("NuevoDoc.xml"));
+            transformer.transform(source, result);
+        } catch (IOException | NumberFormatException | ParserConfigurationException | TransformerConfigurationException | DOMException | SAXException e) {
+        } catch (TransformerException ex) {
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
--- Ejercicio2.1_Tema2/src/ejercicio2/pkg1_tema2/Ejercicio1_2Tema2.java
+++ Ejercicio2.1_Tema2/src/ejercicio2/pkg1_tema2/Ejercicio1_2Tema2.java
@@ -1,37 +1,44 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio2.pkg1_tema2;
 
 import java.io.IOException;
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 /**
  *
- * @author DAM_M
+ * @author DAM
  */
 public class Ejercicio1_2Tema2 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SAXException, ParserConfigurationException, IOException {
-        /*Dado el archivo películas.xml que se adjunta y con la ayuda de DOM, se pide mostrar:
-        • El título de la película,
-        • Año,
-        • Director
-        • Los nombres de los actores principales.*/
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse("peliculas.xm");
+    public static void main(String[] args) {
+
+        try {
+            NodeList nlt = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("peliculas.xml").getElementsByTagName("Titulo");
+            NodeList nlf = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("peliculas.xml").getElementsByTagName("Fecha");
+            NodeList nld = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("peliculas.xml").getElementsByTagName("Director");
+            NodeList nla = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("peliculas.xml").getElementsByTagName("Actores");
+            for (int i = 0; i < nlt.getLength(); i++) {
+                Element e1 = (Element) nlt.item(i);
+                Element e2 = (Element) nlf.item(i);
+                Element e3 = (Element) nld.item(i);
+                Element e4 = (Element) nla.item(i);
+                System.out.println(e1.getTextContent() + "\n");
+                System.out.println(e2.getTextContent() + "\n");
+                System.out.println(e3.getTextContent() + "\n");
+                System.out.println(e4.getTextContent() + "\n");
+            }
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+        }
     }
 
 }
