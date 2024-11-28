9c196384b90e973de1e4abd3c2cd4e6bd8dc4c2a
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-11-28 19:19:16 +0100

    Terminados y comprimidos los archivos

--- 5_Conectores/src/pkg5_conectores/Main.java
+++ 5_Conectores/src/pkg5_conectores/Main.java
@@ -33,16 +33,92 @@ public class Main {
                 opcion = menu();
                 switch (opcion) {
                     case 1:
+                        ResultSet rs = s.executeQuery("SELECT DISTINCT\n"
+                                + "    O.CODIGO_OFICINA\n"
+                                + "FROM OFICINA O\n"
+                                + "    JOIN EMPLEADO E ON E.CODIGO_OFICINA = O.CODIGO_OFICINA\n"
+                                + "WHERE\n"
+                                + "    O.CODIGO_OFICINA NOT IN(\n"
+                                + "        SELECT DISTINCT\n"
+                                + "            E.CODIGO_OFICINA\n"
+                                + "        FROM\n"
+                                + "            EMPLEADO E\n"
+                                + "            JOIN CLIENTE C ON C.CODIGO_EMPLEADO_REP_VENTAS = E.CODIGO_EMPLEADO\n"
+                                + "            JOIN PEDIDO P ON P.CODIGO_CLIENTE = C.CODIGO_CLIENTE\n"
+                                + "            JOIN DETALLE_PEDIDO DP ON DP.CODIGO_PEDIDO = P.CODIGO_PEDIDO\n"
+                                + "            JOIN PRODUCTO PR ON DP.CODIGO_PRODUCTO = PR.CODIGO_PRODUCTO\n"
+                                + "        WHERE\n"
+                                + "            PR.GAMA = 'Frutales'\n"
+                                + "    );");
+                        System.out.println("CODIGO_OFICINA");
+                        System.out.println("--------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1));
+                        }
                         break;
                     case 2:
+                        rs = s.executeQuery("SELECT DISTINCT\n"
+                                + "    C.NOMBRE_CLIENTE\n"
+                                + "FROM\n"
+                                + "    CLIENTE C\n"
+                                + "    JOIN PEDIDO P ON C.CODIGO_CLIENTE = P.CODIGO_CLIENTE\n"
+                                + "    LEFT JOIN PAGO PAGO ON P.CODIGO_CLIENTE = PAGO.CODIGO_CLIENTE\n"
+                                + "WHERE\n"
+                                + "    PAGO.CODIGO_CLIENTE IS NULL;");
+                        System.out.println("NOMBRE_CLIENTE");
+                        System.out.println("--------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1));
+                        }
                         break;
                     case 3:
+                        rs = s.executeQuery("SELECT C.NOMBRE_CLIENTE\n"
+                                + "FROM CLIENTE C\n"
+                                + "    LEFT JOIN PAGO P ON C.CODIGO_CLIENTE = P.CODIGO_CLIENTE\n"
+                                + "WHERE\n"
+                                + "    P.CODIGO_CLIENTE IS NULL;");
+                        System.out.println("NOMBRE_CLIENTE");
+                        System.out.println("--------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1));
+                        }
                         break;
                     case 4:
+                        rs = s.executeQuery("SELECT DISTINCT\n"
+                                + "    C.NOMBRE_CLIENTE\n"
+                                + "FROM CLIENTE C\n"
+                                + "    JOIN PAGO P ON C.CODIGO_CLIENTE = P.CODIGO_CLIENTE;");
+                        System.out.println("NOMBRE_CLIENTE");
+                        System.out.println("--------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1));
+                        }
                         break;
                     case 5:
+                        rs = s.executeQuery("SELECT DISTINCT\n"
+                                + "    PR.NOMBRE\n"
+                                + "FROM\n"
+                                + "    PRODUCTO PR\n"
+                                + "    LEFT JOIN DETALLE_PEDIDO DP ON DP.CODIGO_PRODUCTO = PR.CODIGO_PRODUCTO\n"
+                                + "WHERE\n"
+                                + "    DP.CODIGO_PRODUCTO IS NULL;");
+                        System.out.println("NOMBRE");
+                        System.out.println("--------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1));
+                        }
                         break;
                     case 6:
+                        rs = s.executeQuery("SELECT DISTINCT\n"
+                                + "    PR.NOMBRE\n"
+                                + "FROM\n"
+                                + "    PRODUCTO PR\n"
+                                + "    JOIN DETALLE_PEDIDO DP ON DP.CODIGO_PRODUCTO = PR.CODIGO_PRODUCTO;");
+                        System.out.println("NOMBRE");
+                        System.out.println("--------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1));
+                        }
                         break;
                 }
             } while (opcion != 0);
@@ -63,12 +139,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = -1;
         System.out.println("-Menu-");
-        System.out.println("1. .");
-        System.out.println("2. .");
-        System.out.println("3. .");
-        System.out.println("4. .");
-        System.out.println("5. .");
-        System.out.println("6. .");
+        System.out.println("1. Devuelve las oficinas donde no trabajan ninguno de los empleados que hayan sido los representantes de ventas de algún cliente que haya realizado la compra de algún producto de la gama Frutales.");
+        System.out.println("2. Devuelve un listado con los clientes que han realizado algún pedido pero no han realizado ningún pago.");
+        System.out.println("3. Devuelve un listado que muestre solamente los clientes que no han realizado ningún pago.");
+        System.out.println("4. Devuelve un listado que muestre solamente los clientes que sí han realizado ALGUN pago.");
+        System.out.println("5. Devuelve un listado de los productos que nunca han aparecido en un pedido.");
+        System.out.println("6. Devuelve un listado de los productos que han aparecido en un pedido alguna vez.");
         System.out.println("0. Salir");
 
         do {
--- 6_Conectores/src/pkg6_conectores/Main.java
+++ 6_Conectores/src/pkg6_conectores/Main.java
@@ -33,16 +33,92 @@ public class Main {
                 opcion = menu();
                 switch (opcion) {
                     case 1:
+                        ResultSet rs = s.executeQuery("SELECT DISTINCT\n"
+                                + "    C.NOMBRE_CLIENTE,\n"
+                                + "    SUM(P.TOTAL) AS TOTAL_PAGADO\n"
+                                + "FROM CLIENTE C\n"
+                                + "    LEFT JOIN PAGO P ON C.CODIGO_CLIENTE = P.CODIGO_CLIENTE\n"
+                                + "GROUP BY\n"
+                                + "    C.NOMBRE_CLIENTE;");
+                        System.out.println("NOMBRE_CLIENTE\tTOTAL_PAGADO");
+                        System.out.println("-----------------------------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1) + "\t" + rs.getString(2));
+                        }
                         break;
                     case 2:
+                        rs = s.executeQuery("SELECT DISTINCT\n"
+                                + "    C.NOMBRE_CLIENTE\n"
+                                + "FROM CLIENTE C\n"
+                                + "    LEFT JOIN PEDIDO P ON C.CODIGO_CLIENTE = P.CODIGO_CLIENTE\n"
+                                + "WHERE\n"
+                                + "    YEAR(P.FECHA_PEDIDO) = 2008");
+                        System.out.println("NOMBRE_CLIENTE");
+                        System.out.println("-----------------------------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1));
+                        }
                         break;
                     case 3:
+                        rs = s.executeQuery("SELECT\n"
+                                + "    C.NOMBRE_CLIENTE,\n"
+                                + "    E.NOMBRE AS NOMBRE_REP_VENTA,\n"
+                                + "    E.APELLIDO1 AS APELLIDO_REP_VENTA,\n"
+                                + "    O.TELEFONO AS TELEFONO_OFICINA\n"
+                                + "FROM\n"
+                                + "    CLIENTE C\n"
+                                + "    LEFT JOIN PAGO P ON C.CODIGO_CLIENTE = P.CODIGO_CLIENTE\n"
+                                + "    JOIN EMPLEADO E ON E.CODIGO_EMPLEADO = C.CODIGO_EMPLEADO_REP_VENTAS\n"
+                                + "    JOIN OFICINA O ON O.CODIGO_OFICINA = E.CODIGO_OFICINA\n"
+                                + "WHERE\n"
+                                + "    P.CODIGO_CLIENTE IS NULL;");
+                        System.out.println("NOMBRE_CLIENTE\tNOMBRE_REP_VENTA\tAPELLIDO_REP_VENTA\tTELEFONO_OFICINA");
+                        System.out.println("---------------------------------------------------------------------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4));
+                        }
                         break;
                     case 4:
+                        rs = s.executeQuery("SELECT\n"
+                                + "    C.NOMBRE_CLIENTE,\n"
+                                + "    E.NOMBRE AS NOMBRE_REP_VENTA,\n"
+                                + "    E.APELLIDO1 AS APELLIDO_REP_VENTA,\n"
+                                + "    O.CIUDAD AS CIUDAD_OFICINA\n"
+                                + "FROM\n"
+                                + "    CLIENTE C\n"
+                                + "    JOIN EMPLEADO E ON E.CODIGO_EMPLEADO = C.CODIGO_EMPLEADO_REP_VENTAS\n"
+                                + "    JOIN OFICINA O ON O.CODIGO_OFICINA = E.CODIGO_OFICINA;");
+                        System.out.println("NOMBRE_CLIENTE\tNOMBRE_REP_VENTA\tAPELLIDO_REP_VENTA\tCIUDAD_OFICINA");
+                        System.out.println("---------------------------------------------------------------------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4));
+                        }
                         break;
                     case 5:
+                        rs = s.executeQuery("SELECT E.NOMBRE, E.APELLIDO1, E.PUESTO, O.TELEFONO AS TELEFONO_OFICINA\n"
+                                + "FROM\n"
+                                + "    EMPLEADO E\n"
+                                + "    LEFT JOIN CLIENTE C ON C.CODIGO_EMPLEADO_REP_VENTAS = E.CODIGO_EMPLEADO\n"
+                                + "    JOIN OFICINA O ON O.CODIGO_OFICINA = E.CODIGO_OFICINA\n"
+                                + "WHERE\n"
+                                + "    C.CODIGO_EMPLEADO_REP_VENTAS IS NULL;");
+                        System.out.println("NOMBRE\tAPELLIDO1\tPUESTO\tTELEFONO_OFICINA");
+                        System.out.println("-------------------------------------------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4));
+                        }
                         break;
                     case 6:
+                        rs = s.executeQuery("SELECT O.CIUDAD, COUNT(E.CODIGO_EMPLEADO) AS NUMERO_EMPLEADOS\n"
+                                + "FROM OFICINA O\n"
+                                + "    JOIN EMPLEADO E ON E.CODIGO_OFICINA = O.CODIGO_OFICINA\n"
+                                + "GROUP BY\n"
+                                + "    O.CIUDAD;");
+                        System.out.println("CIUDAD\tNUMERO_EMPLEADOS");
+                        System.out.println("--------------------------");
+                        while (rs.next()) {
+                            System.out.println(rs.getString(1) + "\t\t" + rs.getString(2));
+                        }
                         break;
                 }
             } while (opcion != 0);
@@ -58,19 +134,19 @@ public class Main {
             }
         }
     }
-
+    
     public static int menu() {
         Scanner sc = new Scanner(System.in);
         int n = -1;
         System.out.println("-Menu-");
-        System.out.println("1. .");
-        System.out.println("2. .");
-        System.out.println("3. .");
-        System.out.println("4. .");
-        System.out.println("5. .");
-        System.out.println("6. .");
+        System.out.println("1. Devuelve un listado con los nombres de los clientes y el total pagado por cada uno de ellos. Tenga en cuenta que pueden existir clientes que no han realizado ningún pago.");
+        System.out.println("2. Devuelve el nombre de los clientes que hayan hecho pedidos en 2008 ordenados alfabéticamente de menor a mayor.");
+        System.out.println("3. Devuelve el nombre del cliente, el nombre y primer apellido de su representante de ventas y el número de teléfono de la oficina del representante de ventas, de aquellos clientes que no hayan realizado ningún pago.");
+        System.out.println("4. Devuelve el listado de clientes donde aparezca el nombre del cliente, el nombre y primer apellido de su representante de ventas y la ciudad donde está su oficina.");
+        System.out.println("5. Devuelve el nombre, apellidos, puesto y teléfono de la oficina de aquellos empleados que no sean representante de ventas de ningún cliente.");
+        System.out.println("6. Devuelve un listado indicando todas las ciudades donde hay oficinas y el número de empleados que tiene.");
         System.out.println("0. Salir");
-
+        
         do {
             System.out.print("Opcion:");
             try {
@@ -79,7 +155,7 @@ public class Main {
                 System.out.println("Debe introducir un numero");
             }
         } while (n < 0);
-
+        
         return n;
     }
 }
